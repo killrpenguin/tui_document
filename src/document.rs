b640139fbb67efe25bd01a3594804006e602975e
David <dmcfarland8031@gmail.com>
2025-05-28 06:20:11 -0600

    Changed name to optional. Added From impl. Cleaned out unnecessary extras. Completed first draft of render_document

--- src/document.rs
+++ src/document.rs
@@ -1,16 +1,16 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 use ratatui::{
-    layout::Alignment,
-    style::{Color, Modifier, Style},
-    text::Line,
-    widgets::{Block, Widget},
+    layout::{self, Alignment, Position},
+    style::{self, Styled},
+    text::{self, ToSpan},
+    widgets::{self, Block},
 };
 use ropey::{Error, Rope, RopeBuilder};
 use std::{
     cmp, error, fmt, io,
-    ops::{Bound, RangeBounds},
-    path,
+    ops::{self, Bound, RangeBounds},
+    path::{self, PathBuf},
     sync::atomic::{AtomicU64, Ordering},
 };
 
@@ -18,79 +18,65 @@ use crate::RopeyResult;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Document<'a> {
-    name: std::path::PathBuf,
+    name: Option<path::PathBuf>,
+    modified: bool,
     pub(crate) rope: ropey::Rope,
-    doc_pos: usize,
-    pub(crate) modified: bool,
-    tab_len: u8,
-    hard_tab_indent: bool,
-    pub(crate) alignment: Alignment,
+    pub(crate) visible_range: std::ops::RangeInclusive<usize>,
 
-    pub(crate) block: Option<Block<'a>>,
-    pub(crate) style: Style,
-    pub(crate) line_number_style: Option<Style>,
+    pub(crate) block: Option<widgets::Block<'a>>,
+    pub(crate) style: style::Style,
+    pub(crate) wrap: Option<widgets::Wrap>,
+    pub(crate) scroll: Position,
+    pub(crate) alignment: Alignment,
 }
 
-impl<'a> Document<'_> {
+impl<'a> Document<'a> {
     /// Creates an empty 'Document'.
-    pub fn new(name: &str) -> Self {
+    /// # Example
+    ///
+    /// ```
+    /// use tui_document::Document;
+    ///
+    /// let doc = Document::from_str("Hello\n world!");
+    /// let mut lines = doc.lines();
+
+    /// let empty_rope = Document::new();
+    /// assert_eq!(empty_rope.len_lines(), 2);
+    /// assert_eq!(empty_rope.len_chars(), 0);
+    /// assert_eq!(empty_rope.len_bytes(), 0);
+    /// assert!(empty_rope.is_empty());
+    /// ```
+    pub fn new() -> Self {
         Self {
-            name: path::PathBuf::from(name),
+            name: None,
+            modified: false,
             rope: Rope::new(),
-            doc_pos: 0,
-            modified: false,
-            tab_len: 4,
-            hard_tab_indent: false,
-            alignment: Alignment::Left,
-
+            visible_range: ops::RangeInclusive::new(0, 0),
             block: None,
-            style: Style::default(),
-            line_number_style: None,
+            style: style::Style::default(),
+            wrap: None,
+            scroll: Position::ORIGIN,
+            alignment: Alignment::Left,
         }
     }
-    /// Creates a 'Document' from a string slice.    
-    pub fn from_str(name: &str, text: &str) -> Self {
+    /// Creates a 'Document' from a string slice.
+    ///
+    /// This is a convience constructor.
+    pub fn from_str(text: &str) -> Self {
         Self {
-            name: path::PathBuf::from(name),
+            name: None,
             rope: Rope::from_str(text),
-            doc_pos: 0,
             modified: false,
-            tab_len: 4,
-            hard_tab_indent: false,
-            alignment: Alignment::Left,
+            visible_range: ops::RangeInclusive::new(0, text.len()),
 
             block: None,
-            style: Style::default(),
-            line_number_style: None,
-            //            viewport: Viewport::default(),
-        }
-    }
-    /// Creates a 'Document' from a Vec of strings.
-    pub fn new_lines(name: &str, mut lines: Vec<String>) -> Self {
-        if lines.is_empty() {
-            lines.push(String::new());
-        }
-        let mut builder = RopeBuilder::new();
-        let _ = lines
-            .iter()
-            .map(|line| builder.append(line))
-            .collect::<Vec<_>>();
-        let rope = builder.finish();
-        Self {
-            name: path::PathBuf::from(name),
-            rope,
-            doc_pos: 0,
-            modified: false,
-            tab_len: 4,
-            hard_tab_indent: false,
+            style: style::Style::default(),
+            wrap: None,
+            scroll: Position::ORIGIN,
             alignment: Alignment::Left,
-
-            block: None,
-            style: Style::default(),
-            line_number_style: None,
         }
     }
-    /// Creates a `Document` from a PathBuf.
+    /// Creates a new `Document` from a reader.
     ///
     /// # Errors
     ///
@@ -101,34 +87,20 @@ impl<'a> Document<'_> {
     /// - If the PathBuf can't be identified as a file, an IO error with
     ///    with kind 'NotFound' is returned.
     ///
-    pub fn open_file(path: std::path::PathBuf) -> Result<Self, io::Error> {
-        let rope = match path.metadata() {
-            Ok(md) => {
-                if md.is_file() {
-                    let file = std::fs::File::open(&path)?;
-                    let rope: ropey::Rope = ropey::Rope::from_reader(io::BufReader::new(&file))?;
-                    rope
-                } else {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::NotFound,
-                        "{path:?} is not a file",
-                    ));
-                }
-            }
-            Err(_) => ropey::Rope::new(),
-        };
+    pub fn from_reader<R: io::Read>(reader: R) -> io::Result<Self> {
+        let rope: ropey::Rope = ropey::Rope::from_reader(reader)?;
+        let char_len = rope.len_chars();
         Ok(Self {
-            name: path,
+            name: None,
             rope,
-            doc_pos: 0,
             modified: false,
-            tab_len: 4,
-            hard_tab_indent: false,
-            alignment: Alignment::Left,
+            visible_range: ops::RangeInclusive::new(0, char_len),
 
-            block: None,
-            style: Style::default(),
-            line_number_style: None,
+            block: Some(widgets::Block::<'_>::default()),
+            style: style::Style::default(),
+            wrap: None,
+            scroll: Position::ORIGIN,
+            alignment: Alignment::Left,
         })
     }
     /// Writes the contents of the underlying `Rope` to a writer.
@@ -141,7 +113,7 @@ impl<'a> Document<'_> {
     ///
     /// # Errors
     ///
-    /// - If the writer returns an error, `write_to` stops and returns that
+    /// - If the writer returns an error, `write_file` stops and returns that
     ///   error.
     ///
     /// Note: some data may have been written even if an error is returned.
@@ -152,25 +124,21 @@ impl<'a> Document<'_> {
 
         Ok(())
     }
-    /// Total number of bytes in the `Rope`.
-    ///
-    /// Runs in O(1) time.
+    /// Total number of bytes in the underlying `Rope`.
     pub fn len_bytes(&self) -> usize {
         self.rope.len_bytes()
     }
-    /// Total number of chars in the `Rope`.
-    ///
-    /// Runs in O(1) time.
+    /// Total number of chars in the underlying `Rope`.
     pub fn len_chars(&self) -> usize {
         self.rope.len_chars()
     }
-    /// Total number of lines in the `Rope`.
-    ///
-    /// Runs in O(1) time.
+    /// Total number of lines in the underlying `Rope`.
     pub fn len_lines(&self) -> usize {
         self.rope.len_lines() + 1
     }
     /// Returns true if the rope contains no elements.
+    ///
+    /// # Example
     ///
     /// ```
     /// use tui_document::Document;
@@ -183,11 +151,13 @@ impl<'a> Document<'_> {
         self.rope.len_chars() == 0
     }
     /// An iterator over the underlying `Rope`'s chars starting at a given position.
-    //
+    ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "hi");
+    /// let doc = Document::from_str("hi");
     /// let mut iter = doc.chars_at(0);
     /// assert_eq!(iter.next(), Some('h'));
     /// assert_eq!(iter.next(), Some('i'));
@@ -201,10 +171,13 @@ impl<'a> Document<'_> {
     /// See 'lines' for more details.
     ///
     /// Runs in O(log N) time.
+    ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "Hello\n world!");
+    /// let doc = Document::from_str("Hello\n world!");
     /// let mut lines = doc.lines_at(1);
     /// assert_eq!(lines.next().unwrap().as_str(), Some(" world!"));
     /// assert_eq!(lines.next(), None);
@@ -216,16 +189,19 @@ impl<'a> Document<'_> {
     ///
     /// The returned lines include the line break at the end, if any.
     ///
-    /// A rope will always start with a signle line, even if a line break is not present. // confirm me
+    /// A rope will always start with a signle line, even if a line break is not present.
     ///
     /// The last line is returned even if blank, in which case it
     /// is returned as an empty slice.
     ///
     /// Runs in O(log N) time.
+    ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "Hello\n world!");
+    /// let doc = Document::from_str("Hello\n world!");
     /// let mut lines = doc.lines();
     ///
     /// assert_eq!(lines.next().unwrap().as_str(), Some("Hello\n"));
@@ -258,11 +234,13 @@ impl<'a> Document<'_> {
     /// CRLF pairs and being valid `str` slices there are no guarantees about
     /// where the chunks are split.  For example, they may be zero-sized, they
     /// don't necessarily align with line breaks, etc.
-
+    ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "Hello world!");
+    /// let doc = Document::from_str("Hello world!");
     /// let mut chunk = doc.chunks();
     ///
     /// assert_eq!(chunk.next(), Some("Hello world!"));
@@ -273,10 +251,27 @@ impl<'a> Document<'_> {
     }
     /// Gets an immutable slice of the underlying Rope, using char indices.
     ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "Hello\nworld!");
+    /// let doc = Document::from_str("Hello\nworld!");
+    /// let expected = doc.visible_slice().unwrap().as_str();
+    /// assert_eq!(expected, Some("Hello\nworld!"));
+    /// ```
+    pub fn visible_slice(&self) -> Option<ropey::RopeSlice<'_>> {
+        self.rope
+            .get_slice(self.visible_range.start()..self.visible_range.end())
+    }
+    /// Gets an immutable slice of the underlying Rope, based on the visible range of the document.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use tui_document::Document;
+    ///
+    /// let doc = Document::from_str("Hello\nworld!");
     /// let expected = doc.slice(..6).unwrap().as_str();
     /// assert_eq!(expected, Some("Hello\n"));
     /// ```
@@ -288,10 +283,12 @@ impl<'a> Document<'_> {
     }
     /// Convert cursor position to a character position in the rope.
     ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let doc = Document::from_str("doc_name_goes_here", "Hello \nworld!");
+    /// let doc = Document::from_str("Hello \nworld!");
     ///
     /// assert_eq!(doc.cursor_to_char(0, 1), 1);
     /// assert_eq!(doc.cursor_to_char(1, 1), 8);
@@ -303,10 +300,13 @@ impl<'a> Document<'_> {
     ///
     /// Does not panic.
     ///
+    ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let mut doc = Document::from_str("doc_name_goes_here", "ab");
+    /// let mut doc = Document::from_str("ab");
     /// doc.insert_char('c', 0, 2);
     /// let mut iter = doc.chars_at(2);
     /// assert_eq!(iter.next(), Some('c'));
@@ -327,10 +327,12 @@ impl<'a> Document<'_> {
     ///
     /// Does not panic.
     ///
+    /// # Example
+    ///
     /// ```
     /// use tui_document::Document;
     ///
-    /// let mut doc = Document::from_str("doc_name_goes_here", "Hello beautiful world!");
+    /// let mut doc = Document::from_str("Hello beautiful world!");
     /// doc.insert(0, 22, "\nGoodbye cruel world!");
     ///
     /// let expected = doc.slice(22..).unwrap().as_str();
@@ -370,7 +372,7 @@ impl<'a> Document<'_> {
     ///
     /// ```
     /// use tui_document::Document;
-    /// let mut doc = Document::from_str("doc_name_goes_here", "Hello world!");
+    /// let mut doc = Document::from_str( "Hello world!");
     /// doc.remove(5..);
     /// let expected = doc.slice(..).unwrap().as_str();
     ///
@@ -382,50 +384,111 @@ impl<'a> Document<'_> {
     {
         self.rope.try_remove(char_range)
     }
-    /// Set the style of textarea. By default, textarea is not styled.
-    /// ```
-    /// use ratatui::style::{Style, Color};
+    /// Surrounds the [`Document`] widget with a `Block`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ratatui::widgets::{Block, Paragraph};
     /// use tui_document::Document;
     ///
-    /// let mut doc = Document::from_str("doc_name_goes_here", "Ropes rule!");
-    /// let style = Style::default().fg(Color::Red);
-    /// doc.set_style(style);
-    /// assert_eq!(doc.style(), style);
+    /// let doc = Document::from_str("Hello, world!").block(Block::bordered().title("Document"));
     /// ```
-    pub fn set_style(&mut self, style: Style) {
-        self.style = style;
+    #[must_use = "method moves the value of self and returns the modified value"]
+    pub fn block(mut self, block: widgets::Block<'a>) -> Self {
+        self.block = Some(block);
+        self
     }
-
-    /// Get the current style of textarea.
-    pub fn style(&self) -> Style {
-        self.style
+    /// Sets the wrapping configuration for the widget.
+    ///
+    /// See [`Wrap`](https://docs.rs/ratatui/latest/ratatui/widgets/struct.Wrap.html) for more information on the different options.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ratatui::widgets::{Paragraph, Wrap};
+    /// use tui_document::Document;
+    ///
+    /// let doc = Document::from_str("Hello, world!").wrap(Wrap { trim: true });
+    /// ```
+    #[must_use = "method moves the value of self and returns the modified value"]
+    pub const fn wrap(mut self, wrap: widgets::Wrap) -> Self {
+        self.wrap = Some(wrap);
+        self
     }
-
-    /// Get the block of textarea if exists.
-    pub fn block<'b>(&'b self) -> Option<&'b Block<'b>> {
-        self.block.as_ref()
+    /// Set the text alignment for the given widget.
+    ///
+    /// The alignment is a variant of the Ratatui [`Alignment`](https://docs.rs/ratatui/latest/ratatui/layout/enum.Alignment.html) enum which can be one of Left, Right, or
+    /// Center. If no alignment is specified, the text in a paragraph will be left-aligned.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ratatui::{layout::Alignment, widgets::Paragraph};
+    ///
+    /// let paragraph = Paragraph::new("Hello World").alignment(Alignment::Center);
+    /// ```
+    #[must_use = "method moves the value of self and returns the modified value"]
+    pub const fn alignment(mut self, alignment: Alignment) -> Self {
+        self.alignment = alignment;
+        self
+    }
+    /// Sets the style of the entire widget.
+    ///
+    /// `style` accepts any type that is convertible to [`Style`](https://docs.rs/ratatui/latest/ratatui/style/struct.Style.html)
+    /// (e.g. [`Style`](https://docs.rs/ratatui/latest/ratatui/style/struct.Style.html), [`Color`](https://docs.rs/ratatui/latest/ratatui/style/enum.Color.html) , or
+    /// your own type that implements [`Into<Style>`]).
+    ///
+    /// This applies to the entire widget, including the block if one is present. Any style set on
+    /// the block or text will be added to this style.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ratatui::style::{Style, Stylize};
+    /// use tui_document::Document;
+    ///
+    /// let doc = Document::from_str("Hello, world!").set_style(Style::new().red().on_white());
+    /// ```
+    #[must_use = "method moves the value of self and returns the modified value"]
+    pub fn set_style<S: Into<style::Style>>(mut self, style: S) -> Self {
+        self.style = style.into();
+        self
+    }
+    /// Set the name of the document.
+    ///
+    /// Use rename to replace the name of the document.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use tui_document::Document;
+    ///
+    /// let mut doc = Document::from_str("Hello, world!");
+    /// doc.name("example.txt");
+    /// ```
+    pub fn name<S: AsRef<str>>(&mut self, path: S) {
+        if self.name.is_none() {
+            self.name = Some(PathBuf::from(path.as_ref()))
+        }
+    }
+    /// Rename the document.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use tui_document::Document;
+    ///
+    /// let mut doc = Document::from_str("Hello, world!");
+    /// doc.rename("rename_example.txt");
+    /// ```
+    pub fn rename<S: AsRef<str>>(&mut self, path: S) {
+        self.name = Some(PathBuf::from(path.as_ref()))
     }
 }
 impl<'a> Default for Document<'_> {
     fn default() -> Self {
-        Self::new("tui_document.txt")
-    }
-}
-impl<Iter> From<Iter> for Document<'_>
-where
-    Iter: IntoIterator,
-    Iter::Item: Into<String>,
-{
-    fn from(i: Iter) -> Self {
-        Self::new_lines(
-            "tui_document.txt",
-            i.into_iter().map(|s| s.into()).collect::<Vec<String>>(),
-        )
-    }
-}
-impl<Str: Into<String>> FromIterator<Str> for Document<'_> {
-    fn from_iter<IntoIter: IntoIterator<Item = Str>>(iter: IntoIter) -> Self {
-        iter.into()
+        Self::new()
     }
 }
 impl<'a> Clone for Document<'_> {
@@ -433,15 +496,14 @@ impl<'a> Clone for Document<'_> {
         Self {
             name: self.name.clone(),
             rope: self.rope.clone(),
-            doc_pos: self.doc_pos,
             modified: self.modified,
-            tab_len: self.tab_len,
-            hard_tab_indent: self.hard_tab_indent,
-            alignment: self.alignment,
+            visible_range: self.visible_range.clone(),
 
             block: self.block.clone(),
-            style: self.style,
-            line_number_style: self.line_number_style,
+            style: self.style.clone(),
+            wrap: self.wrap,
+            scroll: self.scroll,
+            alignment: self.alignment,
         }
     }
 }
@@ -453,5 +515,44 @@ impl<'a> std::cmp::Ord for Document<'_> {
 impl<'a> std::cmp::PartialOrd<Document<'a>> for Document<'a> {
     fn partial_cmp(&self, other: &Document) -> Option<cmp::Ordering> {
         Some(self.cmp(other))
+    }
+}
+impl<'a, Iter, S> From<Iter> for Document<'_>
+where
+    Iter: Iterator<Item = S>,
+    S: ToString,
+{
+    /// ```
+    /// use tui_document::Document;
+    ///
+    /// let vec_str = vec!["test1", "test2",];
+    /// let char_arr = ['a', 'b', 'c'];
+    /// let str_arr = ["apple", "banana", "mango"];
+    /// let rope = ropey::Rope::from_str("apple\nbanana");
+    ///
+    /// let from_rope = Document::from(rope.chunks());
+    /// let from_str_arr = Document::from(str_arr.iter());
+    /// let from_char_arr = Document::from(char_arr.iter());
+    /// let from_vec = Document::from(vec_str.iter());
+    /// let from_string = Document::from(String::from("hello").chars());
+    /// let from_str = Document::from("hello".chars());
+    /// ```
+    fn from(value: Iter) -> Self {
+        let mut rope_builder = ropey::RopeBuilder::new();
+        for ele in value {
+            rope_builder.append(&ele.to_string());
+        }
+        let rope = rope_builder.finish();
+        Self {
+            name: None,
+            modified: false,
+            rope,
+            visible_range: ops::RangeInclusive::new(0, 0),
+            block: None,
+            style: style::Style::default(),
+            wrap: None,
+            scroll: Position::ORIGIN,
+            alignment: Alignment::Left,
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,36 +5,52 @@ mod document;
 
 pub use crate::document::Document;
 
-use ratatui::{
-    buffer, layout,
-    widgets::{self, Paragraph},
-};
-// Pass out the underlying Ropey Error messages.
+use ratatui::{buffer, layout, text, widgets::*};
+
 pub type RopeyResult<T> = std::result::Result<T, ropey::Error>;
 
-/// # Disclaimer:
-/// Most of the good code and documentation in this crate has been taken directly from Ropey source code.
-/// My goal in this project is to produce a widget that wraps a great crate and bring it to the Ratitui community. Rather than
-/// mark every function that has taken influence or been copied from the Ropey source code or mark each piece of
-/// documentation that was copied, modified, editied, etc after copying from Ropey source code.
-///
-/// All credit goes to cessen and the Ropey crate.
-
-impl<'a> widgets::Widget for &Document<'_> {
-    fn render(self, area: layout::Rect, buf: &mut buffer::Buffer) {
-        let text = if let Some(txt) = self.slice(..) {
-            if let Some(txt) = txt.as_str() {
-                txt
-            } else {
-                ""
-            }
-        } else {
-            ""
+impl<'a> Document<'_> {
+    fn render_document(&self, text_area: layout::Rect, buf: &mut buffer::Buffer) {
+        let mut visible_slice = match self.visible_slice() {
+            Some(slice) => slice,
+            None => self.rope.slice(..),
         };
-        Paragraph::new(text)
-            .block(widgets::Block::default())
-            .wrap(widgets::Wrap { trim: true })
-            .render(area, buf);
+        match visible_slice.to_line() {
+            Ok(lines) => {
+                let text = text::Text::from(lines);
+                Paragraph::new(text)
+                    .block(Block::default())
+                    .wrap(self.wrap.unwrap_or(Wrap { trim: true }))
+                    .render(text_area, buf);
+            }
+            Err(err) => panic!(
+                "I don't know what I should do if a frame fails to render.\n{}",
+                err
+            ),
+        };
+    }
+}
+
+impl<'a> Widget for Document<'_> {
+    fn render(self, area: layout::Rect, buf: &mut buffer::Buffer) {
+        self.render_document(area, buf);
+    }
+}
+impl<'a> Widget for &Document<'_> {
+    fn render(self, area: layout::Rect, buf: &mut buffer::Buffer) {
+        let _ = &self.render_document(area, buf);
+    }
+}
+pub trait CustomConvertions {
+    fn to_line<'a>(&'a mut self) -> RopeyResult<text::Line<'a>>;
+}
+impl CustomConvertions for ropey::RopeSlice<'_> {
+    fn to_line<'a>(&'a mut self) -> RopeyResult<text::Line<'a>> {
+        let spans = self
+            .chunks()
+            .map(|chunk| text::Span::raw(chunk))
+            .collect::<Vec<text::Span<'_>>>();
+        Ok(text::Line::from(spans))
     }
 }
 
@@ -47,10 +63,7 @@ mod tests {
 
     #[test]
     fn render_widget_integration_test() {
-        let app = Document::from_str(
-            "TestDoc.txt",
-            "This is a line of text.\nThis is another line of text.",
-        );
+        let app = Document::from_str(TEXT);
         let mut terminal = Terminal::new(TestBackend::new(80, 20)).unwrap();
         terminal
             .draw(|frame| frame.render_widget(&app, frame.area()))
@@ -60,4 +73,8 @@ mod tests {
 
     #[test]
     fn test_booyer_moore() {}
+
+    static TEXT: &str = "
+A widget to display some text. It is used to display a block of text. The text can be styled and aligned. It can also be wrapped to the next line if it is too long to fit in the given area.
+    ";
 }
